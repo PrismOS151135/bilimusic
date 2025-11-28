cad057c143b5d56e9b9c9560c01a6a0d045a2f94
PrismOS151135 <19260518568@163.com>
2025-11-28 11:28:35 +0800

    -

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         //noinspection ExpiredTargetSdkVersion
         targetSdk = 26
         versionCode = 2
-        versionName = "2.4"
+        versionName = "2.5"
         multiDexEnabled = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
